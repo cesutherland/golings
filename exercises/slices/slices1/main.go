87c0fef33a986465340a7df7f9c0e20d1e46dbae
Carl Sutherland <carl@humblesoftware.com>
2024-12-07 07:53:45 -0500

    Carl progress.

--- exercises/slices/slices1/main.go
+++ exercises/slices/slices1/main.go
@@ -1,13 +1,13 @@
 // slices1
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	a := make(, 3, 10) // play with length and capacity
+	a := make([]int, 3, 10) // play with length and capacity
+	fmt.Println("values of 'a':", a)
 	fmt.Println("length of 'a':", len(a))
 	fmt.Println("capacity of 'a':", cap(a))
 }
--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,19 +1,22 @@
 // structs2
 // Make me compile!
-//
-// I AM NOT DONE
 package main
 
 import "fmt"
 
+type ContactDetails struct {
+	phone string
+}
+
 type Person struct {
-	// don't just create the phone field here. embed a new struct
-	name string
-	age int
+	name  string
+	age   int
+	phone ContactDetails
 }
 
 func main() {
-	// contactDetails := ContactDetails{}
+	contactDetails := ContactDetails{phone: "+1 555 555 5555"}
 	person := Person{name: "John", age: 32}
+	person.phone = contactDetails
 	fmt.Printf("%s is %d years old and his phone is %s\n", person.name, person.age, person.phone)
 }
